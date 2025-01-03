ddff773c6022e9fea638548a51dd00a5b614f981
bala98812 <94346253+bala98812@users.noreply.github.com>
2025-01-03 11:38:06 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function hamburg() {
+    const navbar = document.querySelector(".dropdown");
+    navbar.classList.add('active');
+}
+
+function cancel() {
+    const navbar = document.querySelector(".dropdown");
+    navbar.classList.remove('active');
+}
